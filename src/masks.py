50b28b75199d39de06910a78d4018327814c1476
AV_Geyer <arbitr705@gmail.com>
2025-08-04 19:47:22 +0300

    еализация логирования: добавлены логеры для модулей masks и utils

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,65 @@
-def get_mask_card_number(card_number: str) -> str:
-    """
-    Маскирует номер карты в формате XXXX XX** **** XXXX.
+from logger_config import setup_logger
 
-    :param card_number: Номер карты (16 цифр)
-    :return: Маскированный номер карты
+logger = setup_logger('masks', 'masks.log')
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Маскирует номер карты в формате XXXX XX** **** XXXX.
+
+    Args:
+        card_number: Номер карты (16 цифр)
+
+    Returns:
+        Маскированный номер карты
+
+    Examples:
+        >>> get_mask_card_number("1234567890123456")
+        '1234 56** **** 3456'
     """
-    card_str = str(card_number)
-    masked_number = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
-    return masked_number
+    logger.debug(f"Начало маскировки номера карты: {card_number}")
+
+    try:
+        card_str = str(card_number)
+        if len(card_str) != 16 or not card_str.isdigit():
+            error_msg = f"Неверный формат номера карты: {card_number}"
+            logger.error(error_msg)
+            raise ValueError(error_msg)
+
+        masked_number = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
+        logger.info(f"Успешная маскировка карты: {card_number} -> {masked_number}")
+        return masked_number
+
+    except Exception as e:
+        logger.exception(f"Ошибка при маскировке карты: {str(e)}")
+        raise
 
 
 def get_mask_account(account_number: str) -> str:
-    """
-    Маскирует номер счета в формате **XXXX.
+    """Маскирует номер счета в формате **XXXX.
 
-    :param account_number: Номер счета
-    :return: Маскированный номер счета
+    Args:
+        account_number: Номер счета
+
+    Returns:
+        Маскированный номер счета
+
+    Examples:
+        >>> get_mask_account("12345678")
+        '**5678'
     """
-    account_str = str(account_number)
-    masked_account = f"**{account_str[-4:]}"
-    return masked_account
+    logger.debug(f"Начало маскировки номера счета: {account_number}")
+
+    try:
+        account_str = str(account_number)
+        if len(account_str) < 4 or not account_str.isdigit():
+            error_msg = f"Неверный формат номера счета: {account_number}"
+            logger.error(error_msg)
+            raise ValueError(error_msg)
+
+        masked_account = f"**{account_str[-4:]}"
+        logger.info(f"Успешная маскировка счета: {account_number} -> {masked_account}")
+        return masked_account
+
+    except Exception as e:
+        logger.exception(f"Ошибка при маскировке счета: {str(e)}")
+        raise
